264bad502da4a27c69022db0ae03f92d86b69928
Aaryansh1 <aaryanshsharma8@gmail.com>
2023-05-21 02:05:45 +0530

    Next.js Sass config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+const withSass = require('@zeit/next-sass');
 
+module.exports = withSass({
+  cssModules: true, // Enable CSS modules if you want
+});
 module.exports = nextConfig
