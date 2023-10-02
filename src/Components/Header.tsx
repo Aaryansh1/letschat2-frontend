acb69ae2ad2094ebb53cc3370abaefb45cccd404
Aaryansh1 <aaryanshsharma8@gmail.com>
2023-10-02 19:44:25 +0530

    Header M

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,20 +2,37 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "../styles/header.module.scss";
 import AuthOverview from "./authentication/AuthOverview";
-
+import Cookies from "js-cookie";
+import { fetchAPI } from "../utils/Api";
+import Router from "next/router";
 function Header() {
+  const useRouter = Router;
   const [userdata, setUserData] = useState(null);
   const [authMode, setAuthMode] = useState("");
   const [showAuth, setShowAuth] = useState(false);
+
   useEffect(() => {
-    const data = localStorage.getItem("userdata");
-    if (data) {
+    const data = localStorage.getItem("userData");
+    console.log(data);
+    if (data && data !== null && data !== undefined) {
       setUserData(JSON.parse(data));
     }
-  }, [userdata]);
+  }, []);
+  const logout = async () => {
+    try {
+      const response: any = await fetchAPI("/users/logout", "POST", {});
+      const data = await response.data;
+      console.log(data);
+      useRouter.reload();
+    } catch (error) {
+      console.log(error);
+    }
+    localStorage.removeItem("userData");
+    setUserData(null);
+    Cookies.remove("token");
+  };
   return (
     <div className={styles.header}>
-      {" "}
       <Image
         style={{
           borderRadius: "9999%",
@@ -39,7 +56,12 @@ function Header() {
       >
         Sign In
       </div>
-      <div className={`${userdata ? styles.signInLogout : styles.displayNone}`}>
+      <div
+        className={`${userdata ? styles.signInLogout : styles.displayNone}`}
+        onClick={() => {
+          logout();
+        }}
+      >
         Logout
       </div>
       {showAuth ? (
